f7b92a2511a2d30ebb45dc70503d83256ee0efe1
DerianCordobaPerez <derianunanleon@gmail.com>
2022-01-29 16:51:22 -0600

    refactor(Root): Refactor code inside layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
 
     <title>{{ $title ?? 'Leon Arte y Moda' }}</title>
 
-    <!-- CUSTOM STYLES -->
+    <!-- Custom styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     @yield('css')
@@ -27,9 +27,7 @@
     </div>
 
     <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
