cd6ede650bec31381daff10b82fa427c534f985f
DerianCordobaPerez <derianunanleon@gmail.com>
2022-01-29 22:28:55 -0600

    refactor(Root): Convert file in main layout for multiple layouts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,29 +5,22 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ $title ?? 'Leon Arte y Moda' }}</title>
+    <title>{{ $title }}</title>
 
-    <!-- Custom styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-    @yield('css')
+    @isset($css)
+        {{ $css }}
+    @endisset
 </head>
 
 <body>
     <div id="app">
-        <!-- Navbar component -->
-        <x-navbar />
-
-        <main class="container p-2">
-            {{ $slot }}
-        </main>
-
-        <!-- Footer component -->
-        <x-footer />
+        {{ $slot }}
     </div>
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    @isset($js)
+        {{ $js }}
+    @endisset
 </body>
 
 </html>
