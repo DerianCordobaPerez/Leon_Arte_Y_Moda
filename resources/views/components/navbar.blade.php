689d9cbc22e27baa4577ff75ae0faf7447793d81
santelizvargas <ushija6@gmail.com>
2022-02-17 02:24:23 -0600

    create home view and navigation bar style

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,23 +1,34 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg fixed-top p-0">
+    <div class="container-fluid mx-5">
+        <a class="navbar-brand" href="#">
+            <img src="{{asset('image/logoLAM.png')}}" class="logo-navbar" alt="" />
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a href="#" class="nav-link"><i class="bi bi-house-fill"></i> Inicio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#"><i class="bi bi-grid-fill"></i> Categorias</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" href="#"><i class="bi bi-images"></i> Galeria</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link" href="#"><i class="bi bi-telephone-outbound-fill"></i> Contáctanos</a>
                 </li>
+            </ul>
+        </div>
+
+        <div class="social-networks">
+            <ul class="d-flex m-0">
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+                <a href=""><i class="bi bi-twitter"></i></a>
             </ul>
         </div>
     </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,70 @@
 <x-common.layout title="León Arte y Moda">
-    Hola
+
+    {{-- Header principal page --}}
+
+    <div class="header">
+        <div class="fondo">
+            <div class="row mx-5 mt-5 pt-5">
+                <div class="col-4 text-title">
+                    <h1>León Arte y Moda</h1>
+                </div>
+
+                <div class="col-8 text-white text-center">
+                    <h2 class="fs-1 fw-bold">¿Quiénes Somos?</h2>
+                    <p class="fs-5 px-5">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident blanditiis placeat ratione
+                        architecto laudantium perspiciatis aut officiis velit veritatis animi nobis molestias, quaerat
+                        dignissimos. Obcaecati sit dolorum inventore. Eaque iusto impedit
+                        dolorum facere incidunt atque aliquid magni culpa obcaecati non.
+                    </p>
+
+                    <div class="buttons">
+                        <button class="btn-welcome">Explore</button>
+                        <button class="btn-welcome">Video</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- End header principal page --}}
+
+    {{-- mision vision y valores --}}
+
+    <div class="vh-100 mision-vision-container d-flex flex-column justify-content-center">
+
+        <div class="about-title" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="1500">
+            <h1>Sobre nosotros</h1>
+        </div>
+
+        <section class="d-flex justify-content-evenly">
+            <div class="text-center size-mvv d-flex flex-column align-items-center" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
+                <img src="{{asset('image/fondo.jpg')}}" class="card-img-top" alt="...">
+                <h4 class="my-3">Mision</h4>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur id nobis nihil ea exercitationem quaerat iusto optio iure adipisci autem.
+            </div>
+
+            <div class="text-center size-mvv d-flex flex-column align-items-center" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
+                <img src="{{asset('image/fondo.jpg')}}" class="card-img-top" alt="...">
+                <h4 class="my-3">Vision</h4>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur id nobis nihil ea exercitationem quaerat iusto optio iure adipisci autem.
+            </div>
+
+            <div class="text-center size-mvv d-flex flex-column align-items-center" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
+                <img src="{{asset('image/fondo.jpg')}}" class="card-img-top" alt="...">
+                <h4 class="my-3">Valores</h4>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur id nobis nihil ea exercitationem quaerat iusto optio iure adipisci autem.
+            </div>
+
+        </section>
+    </div>
+    {{-- .......... --}}
+
+
+
+
 </x-common.layout>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
