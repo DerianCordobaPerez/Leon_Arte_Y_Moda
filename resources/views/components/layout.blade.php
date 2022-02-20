79a62a6799c186e9712ef7209fd4cc84fb622a19
DerianCordobaPerez <derianunanleon@gmail.com>
2022-02-20 16:57:02 -0600

    feat(Root): Create posts folder and complete structure dashboard layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    @isset($meta)
+        {{ $meta }}
+    @endisset
 
     <title>{{ $title }}</title>
 
