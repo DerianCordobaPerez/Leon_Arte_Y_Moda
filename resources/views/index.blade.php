a6d637a238a32f7db2c05300c63e36ea6d4ccec0
santelizvargas <ushija6@gmail.com>
2022-02-22 00:12:16 -0600

    edit style of homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,5 @@
 <x-common.layout title="León Arte y Moda">
+    
     {{-- Main header --}}
     <div class="header">
         <div class="row mx-5 mt-5 pt-5">
@@ -28,34 +29,52 @@
     <div class="vh-100 mision-vision-container d-flex flex-column justify-content-center">
 
         <div class="about-title" data-aos="fade-left" data-aos-easing="linear" data-aos-duration="1500">
-            <h1>Sobre nosotros</h1>
+            <h1 id="title">Cuales son nuestras metas</h1>
+            <h1>Conoce mas sobre nosotros</h1>
         </div>
 
         <section class="d-flex justify-content-evenly">
-            <div class="text-center size-mvv d-flex flex-column align-items-center" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
-                <img src="{{asset('images/fondo.jpg')}}" class="card-img-top" alt="...">
-                <h4 class="my-3">Mision</h4>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur id nobis nihil ea exercitationem quaerat iusto optio iure adipisci autem.
+            <div class="text-center size-mvv d-flex flex-column align-items-center justify-content-center"
+                data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
+                <img src="{{ asset('images/fondo.jpg') }}" class="card-img-top" alt="...">
+                <h3 class="my-3 subtitle-mvv">Mision</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur id nobis nihil ea
+                    exercitationem
+                    quaerat iusto optio iure adipisci autem.</p>
+
             </div>
 
-            <div class="text-center size-mvv d-flex flex-column align-items-center" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
-                <img src="{{asset('images/fondo.jpg')}}" class="card-img-top" alt="...">
-                <h4 class="my-3">Vision</h4>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur id nobis nihil ea exercitationem quaerat iusto optio iure adipisci autem.
+            <div class="text-center size-mvv d-flex flex-column align-items-center justify-content-center"
+                data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
+                <img src="{{ asset('images/fondo.jpg') }}" class="card-img-top" alt="...">
+                <h3 class="my-3 subtitle-mvv">Vision</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur id nobis nihil ea
+                    exercitationem
+                    quaerat iusto optio iure adipisci autem.</p>
             </div>
 
-            <div class="text-center size-mvv d-flex flex-column align-items-center" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
-                <img src="{{asset('images/fondo.jpg')}}" class="card-img-top" alt="...">
-                <h4 class="my-3">Valores</h4>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur id nobis nihil ea exercitationem quaerat iusto optio iure adipisci autem.
+            <div class="text-center size-mvv d-flex flex-column align-items-center justify-content-center"
+                data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1500">
+                <img src="{{ asset('images/fondo.jpg') }}" class="card-img-top" alt="...">
+                <h3 class="my-3 subtitle-mvv">Valores</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur id nobis nihil ea
+                    exercitationem
+                    quaerat iusto optio iure adipisci autem.</p>
             </div>
+
+
         </section>
+
+
     </div>
+
+    {{-- Slides --}}
+    {{-- end slides --}}
 
     <x-slot:js>
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
         <script>
             AOS.init();
         </script>
-    </x-slot>
+        </x-slot>
 </x-common.layout>
