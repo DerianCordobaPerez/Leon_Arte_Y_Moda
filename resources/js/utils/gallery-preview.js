7e8ce539ad3294733aeab9ffff79db0b55b01616
DerianCordobaPerez <derianunanleon@gmail.com>
2022-02-22 18:17:30 -0600

    feat(Root): Create galley page

---
+++ resources/js/utils/gallery-preview.js
@@ -0,0 +1,59 @@
+// Variables
+
+const btnClose = document.querySelector('#btn-close');
+
+const btnPrevious = document.querySelector('#btn-previous');
+
+const btnNext = document.querySelector('#btn-next');
+
+const images = document.querySelectorAll('.gallery__container--image img');
+
+const lightbox = document.querySelector('#preview');
+
+const imageActive = document.querySelector('#active-image');
+
+let imageIndex = 0;
+
+// lightbox
+
+const openLightbox = (e) => {
+    imageActive.src = e.target.src;
+    lightbox.style.display = 'flex';
+    imageIndex = Array.from(images).indexOf(e.target);
+};
+
+images.forEach((image) => {
+    image.addEventListener('click', openLightbox);
+});
+
+// Cerrar lightbox
+
+btnClose.addEventListener('click', () => {
+    lightbox.style.display = 'none';
+});
+
+// Imagen Siguiente
+
+const nextImage = () => {
+    if (imageIndex === images.length - 1) {
+        imageIndex = -1;
+    }
+    imageActive.src = images[imageIndex + 1].src;
+    imageIndex++;
+}
+
+btnNext.addEventListener('click', nextImage);
+
+// Imagen Anterior
+
+const previousImage = () => {
+    if (imageIndex === 0) {
+        imageIndex = images.length;
+    }
+
+    imageActive.src = images[imageIndex - 1].src;
+    imageIndex--;
+}
+
+btnPrevious.addEventListener('click', previousImage);
+s
--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,3 +1,53 @@
-<x-common.layout title="Galeria">
-    Hello world
+<x-common.layout title="Galería">
+    <h1 class="title-gallery">Galería de Imágenes</h1>
+
+    <div class="container gallery__container m-auto">
+        @forelse($posts as $post)
+            <figure class="gallery__container--image">
+                <img src="{{$post->picture}}">
+                <figcaption>$post->title</figcaption>
+                <div class="image__icons--hover">
+                    <i class="bi bi-box-arrow-down"></i>
+                    <i class="bi bi-share-fill"></i>
+                </div>
+            </figure>
+        @empty
+            <p>No hay imágenes en la galería</p>
+        @endforelse
+    </div>
+
+
+    <!-- SECTION PREVIEW  -->
+    <section class="container-preview" id="preview">
+        <!-- CONTENT PREVIEW -->
+        <div class="content-preview">
+            <!-- IMAGE PREVIEW -->
+            <div class="image-container">
+                <img src="https://www.nicaraguadisena.com/wp-content/uploads/2021/08/20210812201752_IMG_2680-1080x720.jpg" alt="galeria de imagenes" id="active-image" />
+                <button id="btn-previous" class="active-btn" type="button"><i class="bi bi-chevron-compact-left"></i></button>
+                <button id="btn-next" class="active-btn" type="button"><i class="bi bi-chevron-compact-right"></i></button>
+            </div>
+
+            <!-- PREVIEW INFORMATION -->
+            <div class="preview-info">
+                <!-- BTN-CLOSE  -->
+                <button id="btn-close" type="button"><i class="bi bi-x-lg"></i></button>
+                <div class="title-content">
+                    <img src="{{asset('images/logoLAM.png')}}" alt="LOGO"/>
+                    <div class="title-preview">
+                        <h5>Leon Arte y Moda</h5>
+                        <span>01 de enero 2022 <i class="bi bi-globe2"></i></span>
+                    </div>
+                </div>
+
+                <div class="main-content">
+                    <img src="{{asset('images/logoLAM.png')}}" alt="LOGO"/>
+                    <div class="description">
+                        <p>Lorem ipsum dolor sit amet</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+
 </x-common.layout>
